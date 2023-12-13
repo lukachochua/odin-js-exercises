9945138a78128630bc72c57ac34014465185ccda
lukachochua <lukachochua@gmail.com>
2023-12-13 22:13:52 +0400

    remove specific charecter from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...letter) {
+    let newArray = [];
 
+    for (let i = 0; i < array.length; i++) {
+        if(!letter.includes(array[i])) {
+            newArray.push(array[i]);
+        }
+    }
+
+    return newArray;
 };
 
 // Do not edit below this line
