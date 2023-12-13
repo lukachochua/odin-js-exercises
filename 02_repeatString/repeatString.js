bc6dfb939f76004c5ca1dfe80ee1e8ebdfb27927
lukachochua <lukachochua@gmail.com>
2023-12-13 20:13:15 +0400

    first two exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+    let counter = 0;
+    let result = '';
 
+    if (num < 0) {
+        return 'ERROR';
+    } else {
+        while (counter < num) {
+            result += string;
+            counter++;
+        }
+        return result;
+
+
+
+    }
 };
 
 // Do not edit below this line
