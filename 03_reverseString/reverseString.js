31ae2213f88ac35372db18188503c83e32082da4
lukachochua <lukachochua@gmail.com>
2023-12-13 20:59:31 +0400

    successfully reverse the string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function (string) {
+    let newString = '';
 
+    for (let i = 0; i < string.length; i++) {
+        newString += string.slice(-1 - i, string.length-i);
+    }
+
+    return newString;
 };
 
 // Do not edit below this line
