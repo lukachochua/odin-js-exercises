45541734cb8b408790a0fe123e3f245110d60741
lukachochua <lukachochua@gmail.com>
2023-12-14 20:39:03 +0400

    temp conversion from F to C and viceversa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (farenhait) {
+  let celcius = (farenhait - 32) * (5/9);
+
+  let result = Number(celcius.toFixed(1));
+
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celcius) {
+  let farenhait = celcius * 1.8 + 32;
+
+  let result = Number(farenhait.toFixed(1));
+
+  return result;
 };
 
 // Do not edit below this line
